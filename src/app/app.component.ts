593bebacd7bec1c13b616f4beb762c49907365dd
mthgbr <mathieu.gebauer@gmail.com>
2020-04-27 10:10:10 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,36 @@
 import { Component } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+
+const TREE_DATA = {
+  Groceries: {
+    'Almond Meal flour': null,
+    'Organic eggs': null,
+    'Protein Powder': null,
+    Fruits: {
+      Apple: null,
+      Berries: ['Blueberry', 'Raspberry'],
+      Orange: null
+    }
+  },
+  Reminders: [
+    'Cook dinner',
+    'Read the Material Design spec',
+    'Upgrade Application to Angular'
+  ]
+};
+
+export class TodoItemNode {
+  children: TodoItemNode[];
+  item: string;
+}
+
+export class TodoItemFlatNode {
+  item: string;
+  level: number;
+  expandable: boolean;
+}
 
 @Component({
   selector: 'my-app',
@@ -6,5 +38,146 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  flatNodeMap = new Map<TodoItemFlatNode, TodoItemNode>();
+  nestedNodeMap = new Map<TodoItemNode, TodoItemFlatNode>();
+  selectedParent: TodoItemFlatNode | null = null;
+  newItemName = '';
+  // sources
+  treeControl: FlatTreeControl<TodoItemFlatNode>;
+  treeFlattener: MatTreeFlattener<TodoItemNode, TodoItemFlatNode>;
+  dataSource: MatTreeFlatDataSource<TodoItemNode, TodoItemFlatNode>;
+  // checked
+  checklistSelection = new SelectionModel<TodoItemFlatNode>(true /* multiple */);
+  previousChecklistSelection: SelectionModel<TodoItemFlatNode>;
+
+  constructor() {
+    this.treeFlattener = new MatTreeFlattener(
+      this.transformer, this.getLevel, this.isExpandable, this.getChildren
+    );
+    this.treeControl = new FlatTreeControl<TodoItemFlatNode>(this.getLevel, this.isExpandable);
+    this.dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+    // keep previous selection
+    this.previousChecklistSelection = this.checklistSelection;
+  }
+
+  getLevel = (node: TodoItemFlatNode) => node.level;
+
+  isExpandable = (node: TodoItemFlatNode) => node.expandable;
+
+  getChildren = (node: TodoItemNode): TodoItemNode[] => node.children;
+
+  hasChild = (_: number, nodeData: TodoItemFlatNode) => nodeData.expandable;
+
+  hasNoContent = (_: number, nodeData: TodoItemFlatNode) => nodeData.item === '';
+
+  transformer = (node: TodoItemNode, level: number) => {
+    const existingNode = this.nestedNodeMap.get(node);
+    const flatNode = existingNode && existingNode.item === node.item
+      ? existingNode
+      : new TodoItemFlatNode();
+    flatNode.item = node.item;
+    flatNode.level = level;
+    flatNode.expandable = !!node.children;
+    this.flatNodeMap.set(flatNode, node);
+    this.nestedNodeMap.set(node, flatNode);
+    return flatNode;
+  }
+
+  checkAll() {
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.treeControl.dataNodes.length; i++) {
+      if (!this.checklistSelection.isSelected(this.treeControl.dataNodes[i])) {
+        this.checklistSelection.toggle(this.treeControl.dataNodes[i]);
+      }
+      this.treeControl.expand(this.treeControl.dataNodes[i]);
+    }
+  }
+
+  /** Whether all the descendants of the node are selected. */
+  descendantsAllSelected(node: TodoItemFlatNode): boolean {
+    const descendants = this.treeControl.getDescendants(node);
+    const descAllSelected = descendants.every(child =>
+      this.checklistSelection.isSelected(child)
+    );
+    return descAllSelected;
+  }
+
+  /** Whether part of the descendants are selected */
+  descendantsPartiallySelected(node: TodoItemFlatNode): boolean {
+    const descendants = this.treeControl.getDescendants(node);
+    const result = descendants.some(child => this.checklistSelection.isSelected(child));
+    return result && !this.descendantsAllSelected(node);
+  }
+
+  /** Toggle the to-do item selection. Select/deselect all the descendants node */
+  todoItemSelectionToggle(node: TodoItemFlatNode): void {
+    // Rules State
+    // this.modifiedRules = true;
+    // this.savedRules = false;
+    // selection
+    this.checklistSelection.toggle(node);
+    const descendants = this.treeControl.getDescendants(node);
+    this.checklistSelection.isSelected(node)
+      ? this.checklistSelection.select(...descendants)
+      : this.checklistSelection.deselect(...descendants);
+
+    // Force update for the parent
+    descendants.every(child =>
+      this.checklistSelection.isSelected(child)
+    );
+    this.checkAllParentsSelection(node);
+  }
+
+  /** Toggle a leaf to-do item selection. Check all the parents to see if they changed */
+  todoLeafItemSelectionToggle(node: TodoItemFlatNode): void {
+    // Rules State
+    // this.modifiedRules = true;
+    // this.savedRules = false;
+    // selection
+    this.checklistSelection.toggle(node);
+    this.checkAllParentsSelection(node);
+  }
+
+  /* Checks all the parents when a leaf node is selected/unselected */
+  checkAllParentsSelection(node: TodoItemFlatNode): void {
+    let parent: TodoItemFlatNode | null = this.getParentNode(node);
+    while (parent) {
+      this.checkRootNodeSelection(parent);
+      parent = this.getParentNode(parent);
+    }
+  }
+
+  /** Check root node checked state and change it accordingly */
+  checkRootNodeSelection(node: TodoItemFlatNode): void {
+    const nodeSelected = this.checklistSelection.isSelected(node);
+    const descendants = this.treeControl.getDescendants(node);
+    const descAllSelected = descendants.every(child =>
+      this.checklistSelection.isSelected(child)
+    );
+    if (nodeSelected && !descAllSelected) {
+      this.checklistSelection.deselect(node);
+    } else if (!nodeSelected && descAllSelected) {
+      this.checklistSelection.select(node);
+    }
+  }
+
+  /* Get the parent node of a node */
+  getParentNode(node: TodoItemFlatNode): TodoItemFlatNode | null {
+    const currentLevel = this.getLevel(node);
+
+    if (currentLevel < 1) {
+      return null;
+    }
+
+    const startIndex = this.treeControl.dataNodes.indexOf(node) - 1;
+
+    for (let i = startIndex; i >= 0; i--) {
+      const currentNode = this.treeControl.dataNodes[i];
+
+      if (this.getLevel(currentNode) < currentLevel) {
+        return currentNode;
+      }
+    }
+    return null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,63 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {
+  MatTableModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatExpansionModule,
+  MatButtonModule,
+  MatIconModule,
+  MatSortModule,
+  MatTabsModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatTreeModule
+} from '@angular/material';
+import {
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSortModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatTreeModule
+  ],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSortModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatTreeModule
+  ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
